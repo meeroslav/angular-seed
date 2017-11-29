5a4c2a3d469d066ea8fff468dfd27d5965ec60a1
meeroslav <meeroslav@yahoo.com>
2017-11-29 13:08:45 +0000

    Auto-generated commit -- Add missing package
    Triggered by commit: https://github.com/meeroslav/angular-seed/commit/b48ef973dc72c84480ab666ec844697c9ec2544d
    Travis build: https://travis-ci.org/meeroslav/angular-seed/builds/308959545

---
+++ app/5.5b89d5a5dd6701b985ad.chunk.js
@@ -0,0 +1,83 @@
+webpackJsonp([5],{
+
+/***/ 806:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+var core_1 = __webpack_require__(0);
+var router_1 = __webpack_require__(77);
+var core_2 = __webpack_require__(44);
+var data_list_component_1 = __webpack_require__(846);
+var http_1 = __webpack_require__(206);
+var common_1 = __webpack_require__(6);
+var shared_module_1 = __webpack_require__(204);
+var routes = [
+    { path: '', component: data_list_component_1.DataListComponent }
+];
+var DataListPageModule = (function () {
+    function DataListPageModule() {
+    }
+    DataListPageModule = __decorate([
+        core_1.NgModule({
+            imports: [
+                common_1.CommonModule,
+                core_2.TranslateModule,
+                http_1.HttpClientModule,
+                shared_module_1.SharedModule,
+                router_1.RouterModule.forChild(routes)
+            ],
+            declarations: [data_list_component_1.DataListComponent]
+        })
+    ], DataListPageModule);
+    return DataListPageModule;
+}());
+exports.DataListPageModule = DataListPageModule;
+
+
+/***/ }),
+
+/***/ 846:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+var core_1 = __webpack_require__(0);
+var DataListComponent = (function () {
+    function DataListComponent() {
+    }
+    DataListComponent = __decorate([
+        core_1.Component({
+            selector: 'data-list-page',
+            styles: [__webpack_require__(847)],
+            template: __webpack_require__(848),
+            host: {
+                'class': 'page'
+            }
+        })
+    ], DataListComponent);
+    return DataListComponent;
+}());
+exports.DataListComponent = DataListComponent;
+
+
+/***/ }),
+
+/***/ 847:
+/***/ (function(module, exports) {
+
+module.exports = ""
+
+/***/ }),
+
+/***/ 848:
+/***/ (function(module, exports) {
+
+module.exports = "<breadcrumb pageTitle=\"Data lists\" pageSubtitle=\"Park\"></breadcrumb>\n\n<h4>Simple list</h4>\n<ul class=\"list-group\">\n  <li class=\"list-group-item\">\n    <span class=\"list-group-item-heading\">Cras justo odio</span>\n    <span class=\"list-group-item-text\">This is just a simple text inside list group item</span>\n  </li>\n  <li class=\"list-group-item\"><span class=\"list-group-item-heading\">Dapibus ac facilisis in</span></li>\n  <li class=\"list-group-item\"><span class=\"list-group-item-heading\">Morbi leo risus</span></li>\n  <li class=\"list-group-item\"><span class=\"list-group-item-heading\">Porta ac consectetur ac</span></li>\n  <li class=\"list-group-item\"><span class=\"list-group-item-heading\">Vestibulum at eros</span></li>\n</ul>\n\n<h4>List with badges</h4>\n<ul class=\"list-group\">\n  <li class=\"list-group-item justify-content-between\" >\n    <span class=\"list-group-item-heading\">Cras justo odio</span>\n    <span class=\"badge-pill badge-warning badge\">14</span>\n  </li>\n  <li class=\"list-group-item justify-content-between\">\n    <span class=\"list-group-item-heading\">Dapibus ac facilisis in</span>\n    <span class=\"badge-pill badge badge-danger\">2</span>\n  </li>\n  <li class=\"list-group-item justify-content-between\">\n    <!--<span class=\"tag tag-pill tag-info float-right\">1</span>-->\n    <span class=\"list-group-item-heading\">Morbi leo risus</span>\n    <span class=\"badge-pill badge badge-info\">1</span>\n  </li>\n</ul>\n\n<h4>Simple with links</h4>\n<div class=\"list-group\">\n<a href=\"#\" class=\"list-group-item justify-content-between active\">\n  Cras justo odio\n  <i class=\"theme-icon-chevron-right float-right\"></i>\n</a>\n<a href=\"#\" class=\"list-group-item justify-content-between\">\n  Dapibus ac facilisis in\n  <i class=\"theme-icon-chevron-right float-right\"></i>\n</a>\n<a href=\"#\" class=\"list-group-item justify-content-between\">\n  Morbi leo risus\n  <i class=\"theme-icon-chevron-right float-right\"></i>\n</a>\n<a href=\"#\" class=\"list-group-item justify-content-between\">\n  Porta ac consectetur ac\n  <i class=\"theme-icon-chevron-right float-right\"></i>\n</a>\n<a href=\"#\" class=\"list-group-item justify-content-between\">\n  Vestibulum at eros\n  <i class=\"theme-icon-chevron-right float-right\"></i>\n</a>\n</div>\n\n<h4>Simple with buttons</h4>\n<div class=\"list-group\">\n\t<button type=\"button\" class=\"list-group-item list-group-item-action\">Cras justo odio</button>\n\t<button type=\"button\" class=\"list-group-item list-group-item-action\">Dapibus ac facilisis in</button>\n\t<button type=\"button\" class=\"theme-icon-add list-group-item list-group-item-action\">\n\t  Morbi leo risus\n\t</button>\n\t<button type=\"button\" class=\"list-group-item list-group-item-action\">Porta ac consectetur ac</button>\n\t<button type=\"button\" class=\"list-group-item list-group-item-action\">Vestibulum at eros</button>\n</div>\n\n"
+
+/***/ })
+
+});
+//# sourceMappingURL=5.5b89d5a5dd6701b985ad.chunk.js.map
