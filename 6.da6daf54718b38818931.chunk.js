c61ff646d3000f08eb05c69735d004224095fd8c
meeroslav <meeroslav@yahoo.com>
2018-01-10 09:37:03 +0000

    Auto-generated commit -- Fix links
    Triggered by commit: https://github.com/meeroslav/angular-seed/commit/db95278af7aec119c29f58dc12929536f3707683
    Travis build: https://travis-ci.org/meeroslav/angular-seed/builds/327156219

---
+++ 6.da6daf54718b38818931.chunk.js
@@ -0,0 +1,90 @@
+webpackJsonp([6],{
+
+/***/ "./src/app/data-list/data-list.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<breadcrumb pageTitle=\"Data lists\" pageSubtitle=\"Park\"></breadcrumb>\n\n<h4>Simple list</h4>\n<ul class=\"list-group\">\n  <li class=\"list-group-item\">\n    <span class=\"list-group-item-heading\">Cras justo odio</span>\n    <span class=\"list-group-item-text\">This is just a simple text inside list group item</span>\n  </li>\n  <li class=\"list-group-item\"><span class=\"list-group-item-heading\">Dapibus ac facilisis in</span></li>\n  <li class=\"list-group-item\"><span class=\"list-group-item-heading\">Morbi leo risus</span></li>\n  <li class=\"list-group-item\"><span class=\"list-group-item-heading\">Porta ac consectetur ac</span></li>\n  <li class=\"list-group-item\"><span class=\"list-group-item-heading\">Vestibulum at eros</span></li>\n</ul>\n\n<h4>List with badges</h4>\n<ul class=\"list-group\">\n  <li class=\"list-group-item justify-content-between\" >\n    <span class=\"list-group-item-heading\">Cras justo odio</span>\n    <span class=\"badge-pill badge-warning badge\">14</span>\n  </li>\n  <li class=\"list-group-item justify-content-between\">\n    <span class=\"list-group-item-heading\">Dapibus ac facilisis in</span>\n    <span class=\"badge-pill badge badge-danger\">2</span>\n  </li>\n  <li class=\"list-group-item justify-content-between\">\n    <!--<span class=\"tag tag-pill tag-info float-right\">1</span>-->\n    <span class=\"list-group-item-heading\">Morbi leo risus</span>\n    <span class=\"badge-pill badge badge-info\">1</span>\n  </li>\n</ul>\n\n<h4>Simple with links</h4>\n<div class=\"list-group\">\n<a href=\"#\" class=\"list-group-item justify-content-between active\">\n  Cras justo odio\n  <i class=\"theme-icon-chevron-right float-right\"></i>\n</a>\n<a href=\"#\" class=\"list-group-item justify-content-between\">\n  Dapibus ac facilisis in\n  <i class=\"theme-icon-chevron-right float-right\"></i>\n</a>\n<a href=\"#\" class=\"list-group-item justify-content-between\">\n  Morbi leo risus\n  <i class=\"theme-icon-chevron-right float-right\"></i>\n</a>\n<a href=\"#\" class=\"list-group-item justify-content-between\">\n  Porta ac consectetur ac\n  <i class=\"theme-icon-chevron-right float-right\"></i>\n</a>\n<a href=\"#\" class=\"list-group-item justify-content-between\">\n  Vestibulum at eros\n  <i class=\"theme-icon-chevron-right float-right\"></i>\n</a>\n</div>\n\n<h4>Simple with buttons</h4>\n<div class=\"list-group\">\n\t<button type=\"button\" class=\"list-group-item list-group-item-action\">Cras justo odio</button>\n\t<button type=\"button\" class=\"list-group-item list-group-item-action\">Dapibus ac facilisis in</button>\n\t<button type=\"button\" class=\"theme-icon-add list-group-item list-group-item-action\">\n\t  Morbi leo risus\n\t</button>\n\t<button type=\"button\" class=\"list-group-item list-group-item-action\">Porta ac consectetur ac</button>\n\t<button type=\"button\" class=\"list-group-item list-group-item-action\">Vestibulum at eros</button>\n</div>\n\n"
+
+/***/ }),
+
+/***/ "./src/app/data-list/data-list.component.scss":
+/***/ (function(module, exports) {
+
+module.exports = ""
+
+/***/ }),
+
+/***/ "./src/app/data-list/data-list.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return DataListComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("./node_modules/@angular/core/esm5/core.js");
+
+var DataListComponent = (function () {
+    function DataListComponent() {
+    }
+    DataListComponent = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Component"])({
+            selector: 'data-list-page',
+            styles: [__webpack_require__("./src/app/data-list/data-list.component.scss")],
+            template: __webpack_require__("./src/app/data-list/data-list.component.html"),
+            host: {
+                'class': 'page'
+            }
+        })
+    ], DataListComponent);
+    return DataListComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/data-list/data-list.module.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "DataListPageModule", function() { return DataListPageModule; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("./node_modules/@angular/core/esm5/core.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_router__ = __webpack_require__("./node_modules/@angular/router/esm5/router.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__ngx_translate_core__ = __webpack_require__("./node_modules/@ngx-translate/core/index.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__data_list_component__ = __webpack_require__("./src/app/data-list/data-list.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__angular_common_http__ = __webpack_require__("./node_modules/@angular/common/esm5/http.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__angular_common__ = __webpack_require__("./node_modules/@angular/common/esm5/common.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__common_shared_shared_module__ = __webpack_require__("./src/app/_common/shared/shared.module.ts");
+
+
+
+
+
+
+
+var routes = [
+    { path: '', component: __WEBPACK_IMPORTED_MODULE_3__data_list_component__["a" /* DataListComponent */] }
+];
+var DataListPageModule = (function () {
+    function DataListPageModule() {
+    }
+    DataListPageModule = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["NgModule"])({
+            imports: [
+                __WEBPACK_IMPORTED_MODULE_5__angular_common__["CommonModule"],
+                __WEBPACK_IMPORTED_MODULE_2__ngx_translate_core__["b" /* TranslateModule */],
+                __WEBPACK_IMPORTED_MODULE_4__angular_common_http__["b" /* HttpClientModule */],
+                __WEBPACK_IMPORTED_MODULE_6__common_shared_shared_module__["a" /* SharedModule */],
+                __WEBPACK_IMPORTED_MODULE_1__angular_router__["a" /* RouterModule */].forChild(routes)
+            ],
+            declarations: [__WEBPACK_IMPORTED_MODULE_3__data_list_component__["a" /* DataListComponent */]]
+        })
+    ], DataListPageModule);
+    return DataListPageModule;
+}());
+
+
+
+/***/ })
+
+});
+//# sourceMappingURL=6.da6daf54718b38818931.chunk.js.map
